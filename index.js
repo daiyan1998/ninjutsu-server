2a45270381554279a019f95c3609f2bc4c051aa7
daiyan1998 <abduddaiyan7@gmail.com>
2023-06-08 22:50:40 +0600

    added selectedClasses Delete api

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const app = express();
-const cors = require("cors");
 require("dotenv").config();
+const cors = require("cors");
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zkohjx0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,12 +23,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    const inClass = client.db("ninjutsuDb").collection("inClass");
-
+    const inClassCollection = client.db("ninjutsuDb").collection("inClass");
+    const selectedClassCollection = client
+      .db("ninjutsuDb")
+      .collection("selectedClass");
+    // NAME: All Data
     app.get("/instructors", async (req, res) => {
-      const result = await inClass.find().toArray();
+      const result = await inClassCollection.find().toArray();
+      res.send(result);
+    });
+
+    // NAME: Selected Class Collection
+    app.post("/selectedClasses", async (req, res) => {
+      const selectedClass = req.body;
+      const result = await selectedClassCollection.insertOne(selectedClass);
+      res.send(result);
+    });
+
+    app.get("/selectedClasses", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      console.log(req.query.email);
+      const result = await selectedClassCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/selectedClasses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedClassCollection.deleteOne(query);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
